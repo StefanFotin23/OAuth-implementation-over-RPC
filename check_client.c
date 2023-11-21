a251dbb5b48ab33f562bffc0843242b48b3d6b12
Stefan Fotin <andreifotin@gmail.com>
2023-11-22 01:08:53 +0200

    Implemented general logic in client

--- check_client.c
+++ check_client.c
@@ -6,21 +6,20 @@
 
 #include "check.h"
 
-int
-main (int argc, char *argv[])
-{
-	// Init host (server address) and client for RPC
-	char *host = "127.0.0.1";
-	CLIENT *clnt = clnt_create (host, CHECKPROG, CHECKVERS, "tcp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		printf("Error occured creating client to communicate with server=%s\n", host);
-		exit (1);
-	}
+#define LINE_MAX_SIZE 128
+#define WORD_SIZE 16
+#define DELIMITER ","
+#define REQUEST "REQUEST"
+#define READ "READ"
+#define MODIFY "MODIFY"
+#define DELETE "DELETE"
+#define EXECUTE "EXECUTE"
+#define INSERT "INSERT"
+#define REQUEST "REQUEST"
 
-	// REQUEST
+// REQUEST
+void process_request(CLIENT *clnt, char *client_id) {
 	// Request Authorization
-	char *client_id;
 	char **auth_token_aux = request_authorization_1(&client_id, clnt);
 	if (auth_token_aux == (char **) NULL) {
 		clnt_perror (clnt, "call failed");
@@ -36,21 +35,77 @@ main (int argc, char *argv[])
 
 	// Request Access Token
 	struct request_access_token_input req_acc_token_in;
+	req_acc_token_in.auth_token = auth_token;
+	req_acc_token_in.client_id = client_id;
 	struct request_access_token_output *req_acc_token_out = request_access_token_1(&req_acc_token_in, clnt);
 	if (req_acc_token_out == (struct request_access_token_output *) NULL) {
 		clnt_perror (clnt, "call failed");
 	}
+}
 
-	// OPERATIONS
-	// Validate Delegated Action
-	struct validate_delegated_action_input validate_delegated_action_1_arg;
-	char **request_response_aux = validate_delegated_action_1(&validate_delegated_action_1_arg, clnt);
-	if (request_response_aux == (char **) NULL) {
-		clnt_perror (clnt, "call failed");
+int
+main (int argc, char *argv[])
+{
+	if (argc < 2) {
+		printf ("usage: %s input file path\n", argv[0]);
+		exit (1);
 	}
-	char *request_response = (char*) &request_response_aux;
+	char *inputFile = argv[1];
+
+	// Init host (server address) and client for RPC
+	char *host = "127.0.0.1";
+	CLIENT *clnt = clnt_create (host, CHECKPROG, CHECKVERS, "tcp");
+	if (clnt == NULL) {
+		clnt_pcreateerror (host);
+		printf("Error occured creating client to communicate with server=%s\n", host);
+		exit (1);
+	}
+
+	// Open the file for reading
+    FILE *file = fopen(inputFile, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;  // Return an error code
+    }
+
+    // Read and print each line from the file
+    char line[LINE_MAX_SIZE];  // Adjust the buffer size as needed
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // Process each line as needed
+		char client_id[WORD_SIZE];
+		char operation[WORD_SIZE];
+		char duration_string[WORD_SIZE];
+		int token_duration;
+		strcpy(client_id, strtok(line, DELIMITER));
+		strcpy(operation, strtok(line, DELIMITER));
+		strcpy(duration_string, strtok(line, DELIMITER));
+		token_duration = atoi(duration_string);
+
+		if (strcmp(operation, REQUEST) == 0) {
+			process_request(clnt, client_id);
+		} else if (
+			strcmp(operation, READ) == 0 ||
+			strcmp(operation, MODIFY) == 0 ||
+			strcmp(operation, DELETE) == 0 ||
+			strcmp(operation, EXECUTE) == 0 ||
+			strcmp(operation, INSERT) == 0 ||
+			strcmp(operation, READ) == 0)
+		{
+			// OPERATIONS
+			// Validate Delegated Action
+			struct validate_delegated_action_input validate_delegated_action_1_arg;
+			char **request_response_aux = validate_delegated_action_1(&validate_delegated_action_1_arg, clnt);
+			if (request_response_aux == (char **) NULL) {
+				clnt_perror (clnt, "call failed");
+			}
+			char *request_response = (char*) &request_response_aux;
+		} else {
+			printf("Operation=%s unexpected!", operation);
+		}
+    }	
 
 	// Before exiting program
+    fclose(file);
 	clnt_destroy (clnt);
 	exit (0);
 }
